cff8074e263e5616d8a6544ac6e58d9db6ca8848
tomas.perez <tperez.alite@hotmail.es>
2024-11-18 21:18:30 +0100

    Cambio texto programa

--- holamundo/Program.cs
+++ holamundo/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+#region Primer Programa
+Console.WriteLine("Esta es una aplicación de consola!!");
+#endregion
+
+
